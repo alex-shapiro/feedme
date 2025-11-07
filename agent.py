b9ab53b04ff7c23d50ec6ba1bfec974b25bf3332
Alex Shapiro <alex@alexshapiro.xyz>
2025-11-07 11:53:53 -0500

    wip

--- agent.py
+++ agent.py
@@ -231,8 +231,12 @@ class FeedMeAgent:
             ep_rewards_b.append(rb)
         ep_rewards_a = mx.array(ep_rewards_a)
         ep_rewards_b = mx.array(ep_rewards_b)
-        print(f"A reward: mean {mx.mean(ep_rewards_a):.4f} +/- {mx.std(ep_rewards_a)}")
-        print(f"B reward: mean {mx.mean(ep_rewards_b):.4f} +/- {mx.std(ep_rewards_b)}")
+        print(
+            f"A reward: mean {mx.mean(ep_rewards_a):.3f} +/- {mx.std(ep_rewards_a):.3f}"
+        )
+        print(
+            f"B reward: mean {mx.mean(ep_rewards_b):.3f} +/- {mx.std(ep_rewards_b):.3f}"
+        )
         print(f"A num actions: {actions_a}")
         print(f"A num actions: {actions_b}")
 
